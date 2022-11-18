bc6ff5c0f607290d18f00cbd20b786eb19894a1a
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-11-18 17:31:47 -0600

    update UserService - register/login

--- src/main/java/com/wonjunkang/dailyboard/persistence/UserRepository.java
+++ src/main/java/com/wonjunkang/dailyboard/persistence/UserRepository.java
@@ -4,4 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.wonjunkang.dailyboard.model.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
+  User findByEmail(String email);
+
+  Boolean existsByEmail(String email);
 }
--- src/main/java/com/wonjunkang/dailyboard/service/UserService.java
+++ src/main/java/com/wonjunkang/dailyboard/service/UserService.java
@@ -3,10 +3,13 @@ package com.wonjunkang.dailyboard.service;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.wonjunkang.dailyboard.model.User;
 import com.wonjunkang.dailyboard.persistence.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class UserService {
   private final UserRepository userRepository;
@@ -16,9 +19,27 @@ public class UserService {
     this.userRepository = userRepository;
   }
 
-  public User save(User user) {
-    User storedUser = userRepository.insert(user);
-    return storedUser;
+  public User createUser(User user) {
+    if (user == null || user.getEmail() == null) {
+      throw new RuntimeException("Invalid entity");
+    }
+
+    String email = user.getEmail();
+    if (userRepository.existsByEmail(email)) {
+      log.warn("Email already exists {}", email);
+      throw new RuntimeException("Email already exists");
+    }
+
+    return userRepository.save(user);
+  }
+
+  public User getByCredentials(String email, String password, PasswordEncoder encoder) {
+    User existUser = userRepository.findByEmail(email);
+
+    if (existUser != null && encoder.matches(password, existUser.getPassword())) {
+      return existUser;
+    }
+    return null;
   }
 
   public Optional<User> findById(String id) {
