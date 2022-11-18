4f14109d65548ce325fa3364977554be110c10c1
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-11-18 13:59:11 -0600

    init project

---
+++ src/main/java/com/wonjunkang/dailyboard/DailyBoardApplication.java
@@ -0,0 +1,13 @@
+package com.wonjunkang.dailyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wonjunkang/dailyboard/DailyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonjunkang.dailyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
