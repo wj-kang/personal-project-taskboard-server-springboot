d5a62f1a1317a16fe4d658f5dfefbff9f6763579
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-11-18 23:11:12 -0600

    add Board model, repostiory

---
+++ src/main/java/com/wonjunkang/dailyboard/model/Board.java
@@ -0,0 +1,25 @@
+package com.wonjunkang.dailyboard.model;
+
+import java.util.List;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document("boards")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board extends BaseModel {
+
+  private String title;
+
+  @DBRef
+  private List<TaskList> lists;
+
+}
+
+
