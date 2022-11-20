8a97bbb315ee5150e41f7adc6b08e99fc631c1a5
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-11-19 18:10:05 -0600

    update security config

--- src/main/java/com/wonjunkang/taskboard/config/WebSecurityConfig.java
+++ src/main/java/com/wonjunkang/taskboard/config/WebSecurityConfig.java
@@ -1,34 +1,33 @@
 package com.wonjunkang.taskboard.config;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.filter.CorsFilter;
 import com.wonjunkang.taskboard.security.JwtAuthenticationFilter;
 
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
-  private JwtAuthenticationFilter JwtAuthenticationFilter;
+  private JwtAuthenticationFilter jwtAuthenticationFilter;
 
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.cors().and().csrf().disable().httpBasic().disable();
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // http security builder
+    http.cors().and()//
+        .csrf().disable()// csrf는 현재 사용하지 않으므로 disabled
+        .httpBasic().disable()// token을 사용하므로 basic 인증 disable
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()//
+        .authorizeRequests()//
+        .antMatchers("/api/user/login", "/api/user/register").permitAll() //
+        .anyRequest().authenticated(); // 이외 경로는 모두 인증
 
-    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
-
-    http.authorizeRequests()//
-        .antMatchers("/api/user/register", "/api/user/login").permitAll()//
-        .anyRequest().authenticated();
-
-    // For each request, do CorsFilter, and then JwtAuthenticationFilter
-    http.addFilterAfter(JwtAuthenticationFilter, CorsFilter.class);
-
-    return http.build();
+    // 매 요청마다 CorsFilter 실행 후에 JWT authentication Filter를 실행
+    http.addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
   }
+
+
 }
