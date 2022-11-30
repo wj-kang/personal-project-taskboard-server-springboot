397477a5f5eb43178de6f780f068af6135930386
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-11-30 16:50:34 -0600

    update task-list controller - PUT/task-drag

--- src/main/java/com/wonjunkang/taskboard/controller/TaskListController.java
+++ src/main/java/com/wonjunkang/taskboard/controller/TaskListController.java
@@ -1,6 +1,7 @@
 package com.wonjunkang.taskboard.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,10 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.wonjunkang.taskboard.dto.ResponseDTO;
 import com.wonjunkang.taskboard.dto.TaskListBaseDTO;
+import com.wonjunkang.taskboard.dto.TaskOrderUpdateDTO;
 import com.wonjunkang.taskboard.model.Board;
+import com.wonjunkang.taskboard.model.Task;
 import com.wonjunkang.taskboard.model.TaskList;
 import com.wonjunkang.taskboard.persistence.BoardRepository;
 import com.wonjunkang.taskboard.persistence.TaskListRepository;
+import com.wonjunkang.taskboard.persistence.TaskRepository;
 
 @RestController
 @RequestMapping("/api/list")
@@ -25,6 +29,9 @@ public class TaskListController {
 
   @Autowired
   private TaskListRepository taskListRepository;
+
+  @Autowired
+  private TaskRepository taskRepository;
 
   @PostMapping
   public ResponseEntity<?> createTaskList(@AuthenticationPrincipal String userId,
@@ -98,6 +105,44 @@ public class TaskListController {
         return ResponseEntity.status(200).body(new TaskListBaseDTO(updated));
       }
       return ResponseEntity.status(200).body(updated);
+      //
+    } catch (Exception e) {
+      ResponseDTO<String> responseDTO = ResponseDTO.<String>builder().error(e.getMessage()).build();
+      return ResponseEntity.badRequest().body(responseDTO);
+    }
+  }
+
+  @PutMapping("/task-drag")
+  public ResponseEntity<?> updateTaskOrder(@AuthenticationPrincipal String userId,
+      @RequestBody TaskOrderUpdateDTO data) {
+    try {
+      Board board = boardRepository.findById(data.getBoardId()).get();
+      List<TaskList> lists = board.getLists();
+
+      if (data.getDestListIdx() == data.getSrcListIdx()) {
+        TaskList list = lists.get(data.getDestListIdx());
+        List<Task> tasks = list.getTasks();
+        Task taskToMove = tasks.get(data.getSrcTaskIdx());
+
+        tasks.remove(taskToMove);
+        tasks.add(data.getDestTaskIdx(), taskToMove);
+        taskListRepository.save(list); // Update TaskList
+      } else {
+        TaskList srcList = lists.get(data.getSrcListIdx());
+        TaskList destList = lists.get(data.getDestListIdx());
+
+        Task taskToMove = srcList.getTasks().get(data.getSrcTaskIdx());
+        taskToMove.setListId(destList.getId());
+        taskRepository.save(taskToMove); // update task
+
+        srcList.getTasks().remove(taskToMove);
+        destList.getTasks().add(data.getDestTaskIdx(), taskToMove);
+
+        taskListRepository.save(srcList); // update task-list
+        taskListRepository.save(destList);
+      }
+
+      return ResponseEntity.status(200).body("ok");
       //
     } catch (Exception e) {
       ResponseDTO<String> responseDTO = ResponseDTO.<String>builder().error(e.getMessage()).build();
