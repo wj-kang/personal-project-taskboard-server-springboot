8a2ab68d37c7fac8388ff334ab28aaa75b851753
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-12-02 13:09:54 -0600

    add handlers for board-drag, list-drag

--- src/main/java/com/wonjunkang/taskboard/controller/BoardController.java
+++ src/main/java/com/wonjunkang/taskboard/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.wonjunkang.taskboard.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.wonjunkang.taskboard.dto.BoardOrderUpdateDTO;
 import com.wonjunkang.taskboard.dto.ResponseDTO;
+import com.wonjunkang.taskboard.dto.TaskListOrderUpdateDTO;
 import com.wonjunkang.taskboard.model.Board;
 import com.wonjunkang.taskboard.model.TaskList;
 import com.wonjunkang.taskboard.model.User;
@@ -52,7 +55,6 @@ public class BoardController {
     if (user.isPresent()) {
       return ResponseEntity.ok().body(user.get().getBoards());
     }
-
     return ResponseEntity.badRequest().body("Invalid Request");
   }
 
@@ -137,4 +139,75 @@ public class BoardController {
     }
   }
 
+  @PutMapping("/board-drag")
+  public ResponseEntity<?> updateBoardOrder(@AuthenticationPrincipal String userId,
+      @RequestBody BoardOrderUpdateDTO data) {
+    try {
+      User user = userRepository.findById(userId).get();
+      List<Board> boards = user.getBoards();
+      reorderList(boards, data.getSrc(), data.getDest());
+      userRepository.save(user);
+
+      return ResponseEntity.status(200).body("ok");
+
+    } catch (Exception e) {
+      ResponseDTO<String> responseDTO = ResponseDTO.<String>builder().error(e.getMessage()).build();
+      return ResponseEntity.badRequest().body(responseDTO);
+    }
+  }
+
+  @PutMapping("/lists-order")
+  public ResponseEntity<?> updateListsOrder(@AuthenticationPrincipal String userId,
+      @RequestBody TaskListOrderUpdateDTO data) {
+    try {
+      Board board = boardRepository.findById(data.getBoardId()).get();
+      if (!board.getOwnerId().equals(userId)) {
+        throw new RuntimeException("Invalid Request");
+      }
+
+      List<TaskList> lists = board.getLists();
+      int currIdx = data.getIndex();
+      int len = lists.size();
+      boolean isLeft = data.isLeft();
+      int destIdx;
+
+      if (isLeft == true) {
+        destIdx = (currIdx - 1 + len) % len;
+      } else {
+        destIdx = (currIdx + 1) % len;
+      }
+
+      reorderList(lists, currIdx, destIdx);
+      boardRepository.save(board);
+
+      return ResponseEntity.status(200).body("ok");
+
+    } catch (Exception e) {
+      ResponseDTO<String> responseDTO = ResponseDTO.<String>builder().error(e.getMessage()).build();
+      return ResponseEntity.badRequest().body(responseDTO);
+    }
+  }
+
+  /* Utility Methods */
+  private <T> void reorderList(List<T> list, int src, int dest) {
+    if (src < 0 || dest < 0 || src >= list.size() || dest >= list.size()) {
+      throw new Error("Invalid Index");
+    }
+
+    if (src < dest) {
+      for (int i = src; i < dest; i++) {
+        swap(list, i, i + 1);
+      }
+    } else {
+      for (int i = src; i > dest; i--) {
+        swap(list, i, i - 1);
+      }
+    }
+  }
+
+  private <T> void swap(List<T> list, int idx1, int idx2) {
+    T temp = list.get(idx1);
+    list.set(idx1, list.get(idx2));
+    list.set(idx2, temp);
+  }
 }
